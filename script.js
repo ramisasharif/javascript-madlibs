35b76d2b82655afbed0ac26eb803798c3a4344d0
Ramisa Sharif <ramisas777@gmail.com>
2024-08-04 22:15:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let word = "girl"
+console.log(word)
+
+
+let schoolTime = 8
+console.log(schoolTime)
+
+
+let exerciseTime = 2
+console.log(exerciseTime)
+
+
+totalTime = schoolTime + exerciseTime
+console.log(totalTime)
+
+
+let tired = true
+console.log(tired)
+
+
+let userName = prompt("enter a name")
+console.log('Enter a name here.')
+
+
+console.log('The ' + word + ', whose name is '+ userName + ', went to school for ' + schoolTime + ' hours. Then, she went to the gym for ' + exerciseTime + '. Because she worked for ' + totalTime + ' hours, it is true that she was very tired.');
+
+
+// the username part shows up in the inspect element console of the document, screenshot included in the assignment post!!!
 
